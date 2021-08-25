2f474865d2f048575774339d1d35725d7fe3a1fe
Kingsley <amaitsakingsley@gmail.com>
2021-08-25 18:08:04 +0300

    Form vue and Table Vue

--- app/components/Form.vue
+++ app/components/Form.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Form</h1>
+  <v-form>
+    <v-text-field v-if = "id" v-model="id" label="Id"></v-text-field>
+    <v-text-field v-model="name" label="Name"></v-text-field>
+    <v-text-field v-model="email" label="Email"></v-text-field>
+    <v-text-field v-model="password" label="Password"></v-text-field>
+    <v-btn @click="submit({id, name, email, password })"> {{ id ? 'Edit' : 'Submit' }}</v-btn>
+  </v-form>
 </template>
 
 <script>
--- app/components/Table.vue
+++ app/components/Table.vue
@@ -1,10 +1,56 @@
 <template>
-  <h1>Table</h1>
+  <v-data-table
+    :headers="headers"
+    :items="users"
+  >
+    <template v-slot:[`item.edit`] = "{ item }">
+      <v-btn color="success" @click="editItem(item)"> Edit</v-btn>
+    </template>
+    <template v-slot:[`item.delete`] = "{ item }"> 
+      <v-btn color = "warning" @click="deleteItem(item.id)"> Delete></v-btn>
+    </template>
+  </v-data-table>
+
 </template>
 
 <script>
 export default {
-
+   data () {
+      return {
+        headers: [
+          {
+            align: 'start',
+            sortable: true,
+            value: 'name',
+          },
+          { text: 'Name', value: 'name' },
+          { text: 'Email', value: 'email' },
+          { text: 'Password', value: 'password' },
+          { text: 'Edit', value: 'edit' },
+          { text: 'Delete', value: 'delete' },
+        ],
+      }
+    },
+    computed: {
+      users() {
+        return this.$store.state.users.data;
+      }
+    },
+    async fetch() {
+      this.$store.commit("users/storeData", (await this.$axios.get('http://localhost:8888/users')).data)
+    },
+    methods: {
+      async deleteItem(id) {
+        await this.$axios.delete(`https://localhost:8888/users/${id}`);
+        this.$store.commit("users/storeData", (await this.$axios.get('https://localhost:8888/users')).data)
+      },
+      async editItem(user) {
+        this.$store.commit("user/storeID", user.id);
+        this.$store.commit("user/storeName", user.name);
+        this.$store.commit("user/storeEmail", user.email);
+        this.$store.commit("user/storePassword", user.password);
+      }
+    }
 }
 </script>
 
